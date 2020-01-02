b53626542e090c85472ceff74b032516f1486195
yefei <yefei@runsdata.com>
2020-01-02 18:05:18 +0800

    IU-2019.2.2 <keppe@DESKTOP-LAJGIEM Create baseRefactoring.xml, ide.general.xml, Enum.java, Interface.java, lombok-plugin.xml, Default.xml, find.xml, ui.lnf.xml, File Header.java, gradle.settings.xml, filetypes.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, AnnotationType.java, GrepConsole.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,16 @@
+/**
+* Copyright(c) Runsdata Technology Co.,Ltd.
+* All Rights Reserved.
+* <p>
+* This software is the confidential and proprietary information of Runsdata
+* Technology Co.,Ltd. ("Confidential Information"). You shall not disclose
+* such Confidential Information and shall use it only in accordance with the
+* terms of the license agreement you entered into with Runsdata Technology Co.,Ltd.
+* For more information about Runsdata, welcome to http://www.runsdata.com
+* <p>
+* project: ${PROJECT_NAME}
+* <p>
+* Revision History:
+* Date       Version         Name        Description
+* ${DATE}   1.0             YeFei      Creation File
+*/
---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,11 @@
+#parse("File Header.java")
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+* Description:
+* 
+* 
+* @author YeFei
+* @date ${DATE} ${TIME}
+*/
+public @interface ${NAME} {
+}
